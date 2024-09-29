994a9bde6d51ddde59b4979f665f6e521c660422
JimenezReyesJoseAntonio <19161308@itoaxaca.edu.mx>
2024-09-29 12:25:44 -0600

    create models and repository

---
+++ src/main/java/com/example/ticket/events/Organizer.java
@@ -0,0 +1,8 @@
+package com.example.ticket.events;
+
+public record Organizer(
+        int id,
+        String name,
+        String description
+) {
+}
---
+++ src/main/java/com/example/ticket/events/Venue.java
@@ -0,0 +1,10 @@
+package com.example.ticket.events;
+
+public record Venue(
+        int id,
+        String nam,
+        String street,
+        String city,
+        String country
+) {
+}
